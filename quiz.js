491035247f2eace9a8feac64c89fe39b0e57ed41
RuqaiyaAwan <101637868+RuqaiyaAwan@users.noreply.github.com>
2025-05-06 22:47:40 +0500

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,22 @@
+// quiz.js
+const quizData = {
+    timer: 2,
+    questions: [
+      {
+        question: "What is the capital of France?",
+        options: ["London", "Berlin", "Paris", "Madrid"],
+        answer: 2 // index of the correct option
+      },
+      {
+        question: "Which language is used for web development?",
+        options: ["Python", "JavaScript", "C++", "Java"],
+        answer: 1
+      },
+      {
+        question: "What is the capital of Pakistan?",
+        options: ["Karachi", "Islamabad", "Lahore", "Peshawar"],
+        answer: 1
+      }
+    ]
+  };
+  
